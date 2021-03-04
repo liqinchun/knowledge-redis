c68a9951089daa13c6d62d58f1b57745a547fd92
liqinchun <liqc@inhand.com.cn>
2021-03-04 15:08:32 +0800

    first commit

---
+++ src/main/java/com/liqnchun/knowledge/redis/KnowledgeRedisApplication.java
@@ -0,0 +1,13 @@
+package com.liqnchun.knowledge.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgeRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowledgeRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liqnchun/knowledge/redis/KnowledgeRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liqnchun.knowledge.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgeRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
